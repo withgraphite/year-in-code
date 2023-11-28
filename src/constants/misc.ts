c609edb0ea1209518a8fc88e215bc9913fe4b193
DexterStorey <dexter@dexterstorey.com>
2023-11-28 15:53:42 -0500

    constants

---
+++ src/constants/misc.ts
@@ -0,0 +1,12 @@
+export const COMMON_LANGUAGES = new Map([
+	['shell', 'bash'],
+	['c++', 'cplusplus'],
+	['c#', 'csharp'],
+	['objective-c', 'objectivec'],
+	['dockerfile', 'docker'],
+	['scss', 'sass'],
+	['vue', 'vuejs'],
+	['html', 'html5'],
+	['css', 'css3'],
+	['jupyter notebook', 'jupyter']
+])
---
+++ src/constants/urls.ts
@@ -0,0 +1,1 @@
+export const GITHUB_GRAPHQL_API = 'https://api.github.com/graphql'
