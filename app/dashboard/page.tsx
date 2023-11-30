3272d621951c7e7fcc3b90dbc3e0c5a2296c4536
Sarim Malik <sarim@rubriclabs.com>
2023-11-29 19:28:41 -0500

    Add seqeuntial chain

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,10 @@
 import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
-import {LLMChain} from 'langchain/chains'
-import {ChatOpenAI} from 'langchain/chat_models/openai'
-import {ChatPromptTemplate} from 'langchain/prompts'
+import {LLMChain, SimpleSequentialChain} from 'langchain/chains'
+import {OpenAI} from 'langchain/llms/openai'
+import {PromptTemplate} from 'langchain/prompts'
 import Auth from 'lib/components/Auth'
 import {cookies} from 'next/headers'
 import {redirect} from 'next/navigation'
-import Player from '~/components/Player'
 import env from '~/env.mjs'
 import {getUserStats} from '~/utils/stats'
 
@@ -37,31 +36,48 @@ export default async function Dashboard() {
 	console.log('Fetching stats...')
 	const githubStats = await getUserStats(session.provider_token)
 
-	// Write Story
-	const chat = new ChatOpenAI({
+	// Init LLM
+	const llm = new OpenAI({
 		temperature: 0,
 		openAIApiKey: env.OPENAI_API_KEY,
 		modelName: 'gpt-4-1106-preview'
 	})
 
-	const chatPrompt = ChatPromptTemplate.fromMessages([
-		[
-			'system',
-			'You are Github Video Writer, an AI tool that is responsible for generating a compelling narative story arch based on a users year in code. It is very important that this video feels personal, motivated by their real activities and highlights what was special about that users year in code. The goal of this video is to make the end user feel seen, valued and have a nostalgic moment of review. You do not need to touch on everything, rather hone in on and focus on the key elements that made this year special.'
-		],
-		['human', 'The GitHub stats for {username} are as follows: {stats}']
-	])
+	// Chain 1: Generate Story
+	const template = `You are Github Video Writer, an AI tool that is responsible for generating a compelling narative story arch based on a users year in code. 
+					  It is very important that this video feels personal, motivated by their real activities and highlights what was special about that users year in code. 
+					  The goal of this video is to make the end user feel seen, valued and have a nostalgic moment of review. You do not need to touch on everything, rather 
+					  hone in on and focus on the key elements that made this year special.
+					  The GitHub stats are as follows: {stats}`
 
-	const storyChain = new LLMChain({
-		prompt: chatPrompt,
-		llm: chat
+	const promptTemplate = new PromptTemplate({
+		template,
+		inputVariables: ['stats']
 	})
 
-	console.log('Writing story... ')
-	const story = await storyChain.call({
-		username: githubStats.username,
-		stats: JSON.stringify(githubStats)
+	const storyChain = new LLMChain({llm, prompt: promptTemplate})
+
+	// Chain: Generate video manifest
+	const remotionTemplate = `You are a video editor, who can take a story and convert it into a manifest files containing a list of frames to be used to create a Remotion (https://remotion.dev) video. Given the story, it is your job to write a JSON manifest for the video.
+ 								Story:
+								{story}
+								The JSON manifest file for Remotion is:`
+	const remotionPromptTemplate = new PromptTemplate({
+		template: remotionTemplate,
+		inputVariables: ['story']
 	})
+	const remotionChain = new LLMChain({
+		llm: llm,
+		prompt: remotionPromptTemplate
+	})
+
+	const overallChain = new SimpleSequentialChain({
+		chains: [storyChain, remotionChain],
+		verbose: true
+	})
+
+	const manifest = await overallChain.run(JSON.stringify(githubStats))
+	console.log(manifest)
 
 	return (
 		<div className='flex min-h-screen flex-col items-center justify-center gap-5'>
@@ -71,7 +87,7 @@ export default async function Dashboard() {
 				authenticated.
 			</p>
 			<Auth session={session} />
-			<Player videoProps={{title: story.text}} />
+			{/* <Player videoProps={{title: story.text}} /> */}
 		</div>
 	)
 }
