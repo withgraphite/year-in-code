b048266f6605046ac54c3b308b049cd0e80ac3bc
DexterStorey <dexter@dexterstorey.com>
2023-12-05 01:20:54 -0500

    cleanup

--- lib/scenes/Stars.tsx
+++ lib/scenes/Stars.tsx
@@ -15,20 +15,6 @@ export default function Stars({
 			durationInFrames={30 * 5}>
 			<div className='absolute flex h-full w-full items-center justify-center bg-black'>
 				<h2 className='mx-48 text-center text-white'>{text}</h2>
-				{new Array(stars).fill(0).map((_, i) => (
-					<div
-						key={i}
-						className='absolute'
-						style={{
-							top: Math.random() * 100 + '%',
-							left: Math.random() * 100 + '%',
-							width: Math.random() * 10 + 'px',
-							height: Math.random() * 10 + 'px',
-							borderRadius: '50%',
-							backgroundColor: 'white'
-						}}
-					/>
-				))}
 			</div>
 		</Sequence>
 	)
