17d258ae6c31024d674703d50937d15709a6ab87
DexterStorey <dexter@dexterstorey.com>
2023-12-05 01:49:45 -0500

    config github images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com'
+			}
+		]
+	}
 }
 
 export default nextConfig
