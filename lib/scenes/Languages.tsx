0b7ff4774ddf158cffd1fd8ffd6963b2ef21d2dd
DexterStorey <dexter@dexterstorey.com>
2023-12-07 07:48:39 -0500

    devicons

--- lib/scenes/Languages.tsx
+++ lib/scenes/Languages.tsx
@@ -10,21 +10,26 @@ export default function Languages({
 	languages: string[]
 }) {
 	return (
-		<Sequence
-			from={from}
-			durationInFrames={30 * 5}>
-			<div className='absolute flex h-full w-full flex-col items-center justify-center gap-5 bg-black'>
-				<h2 className='mx-48 text-center text-white'>{text}</h2>
-				<div className='flex gap-5'>
-					{languages.map(language => (
-						<div
-							className='text-white'
-							key={language}>
-							{language}
-						</div>
-					))}
+		<>
+			<link
+				rel='stylesheet'
+				href='https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css'
+			/>
+			<Sequence
+				from={from}
+				durationInFrames={30 * 5}>
+				<div className='absolute flex h-full w-full flex-col items-center justify-center gap-5 bg-black'>
+					<h2 className='mx-48 text-center text-white'>{text}</h2>
+					<div className='flex gap-5'>
+						{languages.map(language => (
+							<div
+								className={`text-5xl text-white devicon-${language.toLowerCase()}-plain colored`}
+								key={language}
+							/>
+						))}
+					</div>
 				</div>
-			</div>
-		</Sequence>
+			</Sequence>
+		</>
 	)
 }
