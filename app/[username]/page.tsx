6259c6e60053f62e676c7af24af19161ac0d1b06
Sarim Malik <sarim@rubriclabs.com>
2023-12-08 11:44:06 -0500

    Make responsive

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -17,12 +17,12 @@ export default async function Profile({params}: {params: {username: string}}) {
 	const profile = await getProfile(params.username)
 
 	return (
-		<div className='flex min-h-screen flex-col items-center justify-center gap-5 pt-20'>
+		<div className='flex min-h-screen w-full flex-col items-center gap-5 p-5 sm:justify-center'>
 			{/* User exists */}
 			{profile && (
-				<div className='flex max-w-3xl flex-col items-center justify-center gap-5 px-5'>
-					<div className='flex w-full justify-between'>
-						<h1 className='text-white'>{`${params.username}`}</h1>
+				<div className='flex w-full max-w-3xl flex-col justify-center gap-5 pt-20 sm:items-center'>
+					<div className='flex w-full flex-col justify-between gap-5 sm:flex-row sm:gap-0'>
+						<h1 className='text-black dark:text-white'>{`${params.username}`}</h1>
 						<Toolbar session={session} />
 					</div>
 					{profile && (
@@ -36,8 +36,14 @@ export default async function Profile({params}: {params: {username: string}}) {
 
 			{/* User does not exist */}
 			{!profile && (
-				<div>
-					<h2>Oops! User does not exist.</h2>
+				<div className='flex flex-col items-center justify-center gap-5 pt-20 sm:pt-0'>
+					<h2>
+						Oops!{' '}
+						<span className='italic text-black dark:text-white'>
+							{params.username}
+						</span>{' '}
+						does not exist in our database.
+					</h2>
 					<Toolbar session={session} />
 				</div>
 			)}
