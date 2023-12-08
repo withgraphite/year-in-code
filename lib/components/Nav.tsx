2147e30d9185335618f7b31e6619821ecd58d44b
Sarim Malik <sarim@rubriclabs.com>
2023-12-08 16:07:21 -0500

    Fix

--- lib/components/Nav.tsx
+++ lib/components/Nav.tsx
@@ -4,26 +4,26 @@ import {usePathname} from 'next/navigation'
 
 export default function Nav() {
 	const pathname = usePathname()
-	if (pathname !== '/')
-		return (
-			<nav className='absolute top-0 z-50 flex w-full items-baseline justify-around'>
-				<div
-					className={`flex w-full max-w-3xl items-center ${
-						pathname === '/' ? 'justify-end' : 'justify-between'
-					}  p-5`}>
+	if (pathname === '/') return null
+	return (
+		<nav className='absolute top-0 z-50 flex w-full items-baseline justify-around'>
+			<div
+				className={`flex w-full max-w-3xl items-center ${
+					pathname === '/' ? 'justify-end' : 'justify-between'
+				}  p-5`}>
+				<Link
+					className='text-xl font-bold no-underline'
+					href='/'>
+					Year in Code
+				</Link>
+				{pathname !== '/leaderboard' && (
 					<Link
-						className='text-xl font-bold no-underline'
-						href='/'>
-						Year in Code
+						className='no-underline'
+						href='/leaderboard'>
+						Leaderboard
 					</Link>
-					{pathname !== '/leaderboard' && (
-						<Link
-							className='no-underline'
-							href='/leaderboard'>
-							Leaderboard
-						</Link>
-					)}
-				</div>
-			</nav>
-		)
+				)}
+			</div>
+		</nav>
+	)
 }
