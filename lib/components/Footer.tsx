0f0dfb98527444a30178d370c5741469f8745923
Sarim Malik <sarim@rubriclabs.com>
2023-12-08 22:47:17 -0500

    Fix redirect

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -3,7 +3,7 @@ import Graphite from './Graphite'
 export default function Footer() {
 	return (
 		<footer>
-			<h3 className='absolute bottom-2 left-0 z-10 flex w-full items-center justify-center gap-1 text-stone-200'>
+			<h3 className='absolute bottom-2 left-0 z-10 flex w-full items-center justify-center gap-1 text-stone-200 md:bottom-5'>
 				<span className='text-stone-400 dark:text-stone-600'>by</span>
 				<Graphite />
 			</h3>
