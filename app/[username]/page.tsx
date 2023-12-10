19d5e6fc2e28e95fbdc1898574893892d3a12559
Sarim Malik <sarim@rubriclabs.com>
2023-12-10 00:32:30 -0500

    Refactor loading

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,12 +1,15 @@
 import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
 import {cookies} from 'next/headers'
+import {redirect} from 'next/navigation'
 import GitHubButton from '~/components/GitHubButton'
 import Player from '~/components/Player'
 import Toolbar from '~/components/Toolbar'
 import {Stats} from '~/types/github'
 import {Database} from '~/types/supabase'
 import {Manifest} from '~/types/video'
+import generateScenes from '~/utils/generate'
 import {default as getProfile} from '~/utils/profile'
+import {getStats} from '~/utils/stats'
 
 export const dynamic = 'force-dynamic'
 
@@ -15,10 +18,24 @@ export default async function Profile({params}: {params: {username: string}}) {
 	const {
 		data: {session}
 	} = await supabase.auth.getSession()
-	const profile = await getProfile(params.username)
+	let profile = await getProfile(params.username)
+
+	if (session && !profile)
+		if (session.provider_token === null) {
+			// GitHub provider_token is null if a user revisits the page after the token has expired
+			// Supabase does not plan on adding support for this anytime soon
+			// Improvement: https://github.com/supabase/gotrue-js/issues/806 manually make request to GitHub
+			// API and reset the provider token
+			await supabase.auth.signOut()
+			redirect('/')
+		} else {
+			const stats = await getStats(session.provider_token)
+			const scenes = await generateScenes(stats, session)
+			profile = await getProfile(params.username)
+		}
 
 	return (
-		<div className='flex min-h-screen w-full flex-col items-center justify-center gap-5 p-5'>
+		<div className='flex h-screen w-full flex-col items-center justify-center gap-5 p-5'>
 			{/* User exists */}
 			{profile && (
 				<div
