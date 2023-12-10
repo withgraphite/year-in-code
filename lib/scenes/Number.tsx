469f63de69f4925975f6ef8569fa9a9f11696f78
DexterStorey <dexter@dexterstorey.com>
2023-12-09 21:37:56 -0500

    BAD number scene (wip)

--- lib/scenes/Number.tsx
+++ lib/scenes/Number.tsx
@@ -1,4 +1,6 @@
-import {Sequence} from 'remotion'
+import {ThreeCanvas} from '@remotion/three'
+import {Sequence, useCurrentFrame, useVideoConfig} from 'remotion'
+import BigNumber from '~/components/BigNumber'
 
 export default function Number({
 	from,
@@ -9,13 +11,34 @@ export default function Number({
 	text: string
 	number: number
 }) {
+	const {width, height} = useVideoConfig()
+
+	const frame = useCurrentFrame() - from
 	return (
 		<Sequence
 			from={from}
 			durationInFrames={30 * 5}>
 			<div className='absolute flex h-full w-full flex-col items-center justify-center gap-5 bg-black'>
-				<h2 className='mx-48 text-center text-white'>{text}</h2>
-				<div className='flex gap-5'>NUMBER {number}</div>
+				<div className='absolute h-full w-full'>
+					<ThreeCanvas
+						orthographic={false}
+						width={width}
+						height={height}
+						// style={{
+						// 	backgroundColor: 'black'
+						// }}
+						camera={{fov: 75, position: [0, 0, 470]}}>
+						<BigNumber
+							count={frame}
+							number={number}
+						/>
+					</ThreeCanvas>
+				</div>
+				<div
+					style={{opacity: 1}}
+					className='z-10'>
+					<h1 className='text-4xl text-white'>{text}</h1>
+				</div>
 			</div>
 		</Sequence>
 	)
