4a2b6b13be97a5c6db41da3e800a47084a404555
Sarim Malik <sarim@rubriclabs.com>
2023-12-11 15:14:55 -0500

    Simplify tooltip

--- lib/components/Tooltip.tsx
+++ lib/components/Tooltip.tsx
@@ -10,7 +10,7 @@ export default function Tooltip({
 	return (
 		<div className='group relative flex justify-center'>
 			{children}
-			<span className='absolute bottom-[120%] w-32 scale-0 rounded-md border-2 border-black bg-white/80 px-2 py-1 text-center text-black transition-all delay-200 group-hover:scale-100'>
+			<span className='absolute bottom-[100%] w-32 scale-0 rounded-md px-2 py-1 text-center text-black transition-all delay-200 group-hover:scale-100'>
 				{body}
 			</span>
 		</div>
