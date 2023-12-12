a70622e0bfb0727dd1acebc973e610d4f49a65d3
Sarim Malik <sarim@rubriclabs.com>
2023-12-11 23:29:16 -0500

    Ensure footer does not show on leaderboard

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -1,10 +1,14 @@
+'use client'
+import {usePathname} from 'next/navigation'
 import Graphite from './Graphite'
 
 export default function Footer() {
-	return (
-		<footer className='absolute bottom-2 left-0 z-10 flex w-full items-center justify-center gap-1.5 text-xl text-stone-200 md:bottom-5'>
-			<span className='text-stone-400'>by</span>
-			<Graphite />
-		</footer>
-	)
+	const pathname = usePathname()
+	if (pathname !== '/leaderboard')
+		return (
+			<footer className='absolute bottom-2 left-0 z-10 flex w-full items-center justify-center gap-1.5 text-xl text-stone-200 md:bottom-5'>
+				<span className='text-stone-400'>by</span>
+				<Graphite />
+			</footer>
+		)
 }
