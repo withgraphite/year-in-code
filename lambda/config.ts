8e89d894bd3941fe74a0257d2a474f9e57b261ff
DexterStorey <dexter@dexterstorey.com>
2023-12-14 02:31:37 -0500

    TEMP 900 Frames config

--- lambda/config.ts
+++ lambda/config.ts
@@ -4,4 +4,4 @@ export const RAM = 3008
 export const DISK = 2048
 export const TIMEOUT = 240
 export const COMP_NAME = 'wrapped'
-export const FRAMES_PER_LAMBDA = 30
+export const FRAMES_PER_LAMBDA = 900
