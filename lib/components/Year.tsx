ce1d44f52fe43d372336462af43baa663ff4a123
Sarim Malik <sarim@rubriclabs.com>
2023-12-14 22:41:54 -0500

    Fix

--- lib/components/Year.tsx
+++ lib/components/Year.tsx
@@ -8,7 +8,7 @@ export default function Year() {
 				initial={{opacity: 0, scale: 0.8}}
 				animate={{opacity: 1, scale: 1}}
 				transition={{duration: 0.3}}
-				className='flex flex-col text-[280px] text-black/50 md:text-[350px] lg:flex-row lg:gap-[300px] lg:text-[450px]'>
+				className='flex flex-col gap-20 text-[280px] text-black/50 md:text-[350px] lg:flex-row lg:gap-[300px] lg:text-[450px]'>
 				<div className='flex gap-5 md:gap-10 lg:gap-0'>
 					<span>2</span>
 					<span>0</span>
