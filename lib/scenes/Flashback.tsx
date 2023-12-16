5574a8b9a113e6a4236f2e339b8cf151f2a0c8b9
DexterStorey <dexter@dexterstorey.com>
2023-12-16 03:22:05 -0500

    intro tweaks

--- lib/scenes/Flashback.tsx
+++ lib/scenes/Flashback.tsx
@@ -1,8 +1,8 @@
 import {useCurrentFrame} from 'remotion'
+import FastTravel from '~/environment/FastTravel'
 import Canvas from '../3d/Canvas'
 import Camera from '../camera/Camera'
 import WormHole from '../environment/WarpDrive'
-import useRamp from '../keyframes/useRamp'
 import FadeIn from '../transitions/FadeIn'
 import Sequence from '../video/Sequence'
 
@@ -10,36 +10,74 @@ export default function Flashback({dateFrom, dateTo, text, from}) {
 	const frame = useCurrentFrame() - from
 
 	const secondsPerFrame =
-		(new Date(dateFrom).getTime() - new Date(dateTo).getTime()) / 60
+		(new Date(dateFrom).getTime() - new Date(dateTo).getTime()) / 119
 
 	const date =
-		frame < 30
+		frame < 32
 			? dateFrom
-			: frame < 90
+			: frame < 148
 				? new Date(dateFrom).getTime() - (frame - 30) * secondsPerFrame
 				: dateTo
 
-	const speed = useRamp(2, -2, 150, frame)
+	// const speed = useRamp(2, -2, 150, frame)
 
 	return (
 		<Sequence
 			from={from}
-			transitionIn='fade'
+			transitionIn='warp'
 			transitionOut='warp'
 			background={
-				<Canvas
-					frame={frame}
-					camera={
-						<Camera
-							position={[0, 0, 400]}
-							fov={50 + frame / 30}
+				<>
+					<Canvas
+						frame={frame}
+						camera={
+							<Camera
+								position={[0, 0, 400]}
+								fov={100}
+							/>
+						}>
+						<FastTravel
+							tick={frame}
+							speed={10 + frame / 3}
 						/>
-					}>
-					<WormHole
-						tick={frame}
-						speed={speed}
-					/>
-				</Canvas>
+					</Canvas>
+					<FadeIn
+						frame={frame}
+						duration={60}
+						delay={15}>
+						<Canvas
+							frame={frame}
+							camera={
+								<Camera
+									position={[0, 0.01, 400]}
+									fov={30 + frame / 4}
+								/>
+							}>
+							<WormHole
+								tick={frame}
+								speed={0.15 - frame * 0.0001}
+							/>
+						</Canvas>
+					</FadeIn>
+					{/* <FadeIn
+						frame={frame}
+						duration={30}
+						delay={10}>
+						<Canvas
+							frame={frame}
+							camera={
+								<Camera
+									position={[0, 0.5 - frame / 130, 300 - frame / 1]}
+									fov={50 - frame / 3}
+								/>
+							}>
+							<WormHole
+								tick={frame}
+								speed={0.25 - frame * 0.0005}
+							/>
+						</Canvas>
+					</FadeIn> */}
+				</>
 			}
 			content={
 				<>
@@ -47,7 +85,7 @@ export default function Flashback({dateFrom, dateTo, text, from}) {
 						frame={frame}
 						delay={30}>
 						<h2 className='text-white/80'>{text}</h2>
-						<h1 className='text-center'>
+						<h1 className='text-center font-mono'>
 							{new Date(date).toLocaleDateString('en-US', {
 								year: 'numeric',
 								month: '2-digit',
@@ -55,6 +93,18 @@ export default function Flashback({dateFrom, dateTo, text, from}) {
 							})}
 						</h1>
 					</FadeIn>
+					<div
+						className='absolute z-10 h-full w-full bg-white/30'
+						style={{display: frame === 140 ? 'block' : 'none'}}
+					/>
+					<div
+						className='absolute z-10 h-full w-full bg-white/50'
+						style={{display: frame === 145 ? 'block' : 'none'}}
+					/>
+					<div
+						className='absolute z-10 h-full w-full bg-white/50'
+						style={{display: frame === 149 ? 'block' : 'none'}}
+					/>
 				</>
 			}
 		/>
--- lib/scenes/Intro.tsx
+++ lib/scenes/Intro.tsx
@@ -1,4 +1,6 @@
 import {useCurrentFrame} from 'remotion'
+import FadeOut from '~/transitions/FadeOut'
+import FastTravel from '../environment/FastTravel'
 import Canvas from './../3d/Canvas'
 import Camera from './../camera/Camera'
 import Space from './../environment/Space'
@@ -15,43 +17,101 @@ export default function Intro({title, from, planet}) {
 	const transitionDuration = 15 // Duration of the transition period in frames
 
 	const fov =
-		frame < 3.5 * 30
+		frame < 2.5 * 30
 			? linearFov
-			: frame < 3.5 * 30 + transitionDuration
+			: frame < 2.5 * 30 + transitionDuration
 				? linearFov +
 					(exponentialFovStart - linearFov) *
-						((frame - 3.5 * 30) / transitionDuration)
+						((frame - 2.5 * 30) / transitionDuration)
 				: exponentialFovStart +
-					Math.pow((frame - 3.5 * 30 - transitionDuration) / exponentialRate, 2.7)
+					Math.pow((frame - 2.5 * 30 - transitionDuration) / exponentialRate, 2.7)
 
 	return (
 		<Sequence
 			from={from}
 			transitionIn='fade'
-			transitionOut='fade'
+			transitionOut='warp'
 			background={
-				<Canvas
-					frame={frame}
-					camera={
-						<Camera
-							position={[0, 0, (300 / (50 + fov / 3)) * 2 * 50]}
-							fov={fov}
-						/>
-					}>
-					<Space tick={frame} />
-					<Planet
-						tick={frame}
-						planet={planet}
-					/>
-				</Canvas>
+				<>
+					<div style={{opacity: frame < 120 ? 1 : 0}}>
+						<Canvas
+							frame={frame}
+							camera={
+								<Camera
+									position={[0, 0, (300 / (30 + fov * 1.1)) * 2 * 50]}
+									fov={fov}
+								/>
+							}>
+							<Space tick={frame} />
+							<Planet
+								tick={frame}
+								planet={planet}
+							/>
+						</Canvas>
+					</div>
+					<div
+						style={{
+							opacity: frame < 120 || frame > 160 ? 0 : 1,
+							display: frame < 120 || frame > 160 ? 'none' : 'block'
+						}}>
+						<Canvas
+							frame={frame}
+							camera={
+								<Camera
+									position={[0, 0, 100]}
+									fov={1 + 6 * (frame - 120)}
+								/>
+							}>
+							<FastTravel
+								tick={frame}
+								speed={-2.5 + 0.02 * (frame - 125)}
+							/>
+						</Canvas>
+					</div>
+					<div
+						style={{
+							opacity: frame < 135 ? 0 : 1,
+							display: frame < 135 ? 'none' : 'block'
+						}}>
+						{/* <Canvas
+							frame={frame}
+							camera={
+								<Camera
+									position={[0, 0, 1000]}
+									fov={90}
+								/>
+							}>
+							
+							<Space tick={frame * frame} />
+						</Canvas> */}
+						{/* <Canvas
+							frame={frame}
+							camera={
+								<Camera
+									position={[0, 0, 1300]}
+									fov={0.1}
+								/>
+							}>
+							<FastTravel
+								tick={frame}
+								speed={-0.5 + 0.01 * (frame - 135)}
+							/>
+						</Canvas> */}
+					</div>
+				</>
 			}
 			content={
 				<>
 					<FadeIn
 						frame={frame}
 						delay={60}>
-						<h1 className='text-4xl'>{title}</h1>
-						<h2 className='text-center text-2xl'>by Graphite</h2>
+						<FadeOut
+							frame={frame}
+							duration={10}
+							delay={100}>
+							<h1 className='text-4xl'>{title}</h1>
+							<h2 className='text-center text-2xl'>by Graphite</h2>
+						</FadeOut>
 					</FadeIn>
 				</>
 			}
