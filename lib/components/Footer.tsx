dad9564cab84b1f43cab55dab2e901c064b02ba9
Sarim Malik <sarim@rubriclabs.com>
2023-12-17 20:43:02 -0500

    Keep UX consistent

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -11,13 +11,13 @@ export default function Footer() {
 				<Link
 					href={META.github}
 					target='_blank'
-					className='group border-r border-white pr-3 no-underline hover:underline'>
+					className='group border-r border-white pr-3 no-underline'>
 					<span className='text-lg font-thin'>See code</span>
 				</Link>
 				<Link
 					href={META.blog}
 					target='_blank'
-					className='group relative flex items-center pl-3 no-underline hover:underline'>
+					className='group relative flex items-center pl-3 no-underline'>
 					<span className='text-lg font-thin'>Learn more</span>
 				</Link>
 			</footer>
