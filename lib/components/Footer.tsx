2b60cd13aca21668dcdf2d993231f3a03f2a6f04
Sarim Malik <sarim@rubriclabs.com>
2023-12-17 20:32:56 -0500

    Prevent footer from rendering during loading state

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -1,21 +1,25 @@
+'use client'
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 import {META} from '~/constants/metadata'
 
 export default function Footer() {
-	return (
-		<footer className='absolute bottom-0 flex w-full items-center justify-center p-5 text-white'>
-			<Link
-				href={META.github}
-				target='_blank'
-				className='group border-r border-white pr-3 no-underline hover:underline'>
-				<span className='text-lg font-thin'>See code</span>
-			</Link>
-			<Link
-				href={META.blog}
-				target='_blank'
-				className='group relative flex items-center pl-3 no-underline hover:underline'>
-				<span className='text-lg font-thin'>Learn more</span>
-			</Link>
-		</footer>
-	)
+	const pathname = usePathname()
+	if (pathname !== '/loading')
+		return (
+			<footer className='absolute bottom-0 flex w-full items-center justify-center p-5 text-white'>
+				<Link
+					href={META.github}
+					target='_blank'
+					className='group border-r border-white pr-3 no-underline hover:underline'>
+					<span className='text-lg font-thin'>See code</span>
+				</Link>
+				<Link
+					href={META.blog}
+					target='_blank'
+					className='group relative flex items-center pl-3 no-underline hover:underline'>
+					<span className='text-lg font-thin'>Learn more</span>
+				</Link>
+			</footer>
+		)
 }
