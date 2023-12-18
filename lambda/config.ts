5ee3e5bc961af7e64edf0619e69391728973cd96
DexterStorey <dexter@dexterstorey.com>
2023-12-18 08:29:51 -0500

    render config

--- lambda/config.ts
+++ lambda/config.ts
@@ -1,7 +1,7 @@
 export const REGION = 'us-east-1'
 export const SITE_NAME = 'github-wrapped'
-export const RAM = 3009
+export const RAM = 4096
 export const DISK = 2048
 export const TIMEOUT = 900
 export const COMPOSITION_NAME = 'wrapped'
-export const FRAMES_PER_LAMBDA = 30
+export const FRAMES_PER_LAMBDA = 60
