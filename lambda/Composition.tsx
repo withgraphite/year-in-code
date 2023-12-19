28f89a025e450ed6b1f617b0e0766d7229686c96
DexterStorey <dexter@dexterstorey.com>
2023-12-19 06:38:38 -0500

    clean up

--- lambda/Composition.tsx
+++ lambda/Composition.tsx
@@ -7,22 +7,6 @@ import {Stats} from '../lib/types/github'
 import {Manifest} from '../lib/types/video'
 import {COMPOSITION_NAME} from './config'
 
-// import {continueRender, delayRender} from 'remotion'
-
-// const waitForFont = delayRender()
-// const font = new FontFace(
-// 	`Matter`,
-// 	`url('${staticFile('fonts/MatterSQ-Bold.otf')}') format('otf')`
-// )
-
-// font
-// 	.load()
-// 	.then(() => {
-// 		document.fonts.add(font)
-// 		continueRender(waitForFont)
-// 	})
-// 	.catch(err => console.log('Error loading font', err))
-
 export default function Composition() {
 	return (
 		<RemotionComposition
