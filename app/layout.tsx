17810f1d826239478833cb24053100f72d6e365c
Sarim Malik <sarim@rubriclabs.com>
2023-12-19 02:36:37 -0500

    Add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/next'
 import 'devicon'
 import BackgroundGrid from 'lib/components/BackgroundGrid'
 import {Metadata} from 'next'
@@ -50,6 +51,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 					{children}
 					<Footer />
 					<Analytics />
+					<SpeedInsights />
 				</div>
 			</body>
 		</html>
