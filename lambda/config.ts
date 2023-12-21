8458fa34f6fc514bfa0643337f2244aab6cd0208
DexterStorey <dexter@dexterstorey.com>
2023-12-21 00:25:05 -0500

    2 min render

--- lambda/config.ts
+++ lambda/config.ts
@@ -1,7 +1,7 @@
 export const REGION = 'us-east-1'
 export const SITE_NAME = 'github-wrapped'
-export const RAM = 4008
-export const DISK = 2048
+export const RAM = 4008 * 2
+export const DISK = 2048 * 2
 export const TIMEOUT = 900
 export const COMPOSITION_NAME = 'wrapped'
 export const FRAMES_PER_LAMBDA = 30
