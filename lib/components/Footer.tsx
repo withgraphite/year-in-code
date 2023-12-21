7436202262fb445732ce6b7a49395f078e59bd77
Sarim Malik <sarim@rubriclabs.com>
2023-12-21 01:49:44 -0500

    Ensure there is a sign out button on the loading page

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -3,32 +3,41 @@ import {track} from '@vercel/analytics'
 import Link from 'next/link'
 import {usePathname} from 'next/navigation'
 import {META} from '~/constants/metadata'
+import SignOutButton from './SignOutButton'
 
 export default function Footer() {
 	const pathname = usePathname()
-	if (pathname !== '/loading')
+	if (pathname === '/loading')
 		return (
 			<footer className='absolute bottom-0 flex w-full items-center justify-center p-5 text-white'>
-				<Link
-					href={META.github}
-					target='_blank'
-					className='group border-r border-white pr-3 no-underline'>
-					<button
-						className='border-none bg-transparent p-0 text-lg font-thin text-white hover:bg-transparent hover:text-white'
-						onClick={() => track('Visit GitHub')}>
-						See code
-					</button>
-				</Link>
-				<Link
-					href={META.blog}
-					target='_blank'
-					className='group relative flex items-center pl-3 no-underline'>
-					<button
-						onClick={() => track('Visit blog')}
-						className='border-none bg-transparent p-0 text-lg font-thin text-white hover:bg-transparent hover:text-white'>
-						Learn more
-					</button>
-				</Link>
+				<SignOutButton
+					className='text-white'
+					expand
+				/>
 			</footer>
 		)
+	return (
+		<footer className='absolute bottom-0 flex w-full items-center justify-center p-5 text-white'>
+			<Link
+				href={META.github}
+				target='_blank'
+				className='group border-r border-white pr-3 no-underline'>
+				<button
+					className='border-none bg-transparent p-0 text-lg font-thin text-white hover:bg-transparent hover:text-white'
+					onClick={() => track('Visit GitHub')}>
+					See code
+				</button>
+			</Link>
+			<Link
+				href={META.blog}
+				target='_blank'
+				className='group relative flex items-center pl-3 no-underline'>
+				<button
+					onClick={() => track('Visit blog')}
+					className='border-none bg-transparent p-0 text-lg font-thin text-white hover:bg-transparent hover:text-white'>
+					Learn more
+				</button>
+			</Link>
+		</footer>
+	)
 }
