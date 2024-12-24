dbd9655888799fea842d5e8a436a283be590eb06
Chang Liu <chang@graphite.dev>
2024-12-24 12:21:48 -0800

    fix(images): allow private avatar url

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
 			{
 				protocol: 'https',
 				hostname: 'avatars.githubusercontent.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'private-avatars.githubusercontent.com'
 			}
 		]
 	}
