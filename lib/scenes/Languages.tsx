4b87b81316330cc23d416b7b5d9f837c9af96346
Chang Liu <chang@graphite.dev>
2025-01-06 22:10:54 -0500

    fix(languages): parity with devicons names

--- lib/scenes/Languages.tsx
+++ lib/scenes/Languages.tsx
@@ -1,4 +1,5 @@
 import {useCurrentFrame} from 'remotion'
+import {getLanguage} from '../utils/language'
 
 export default function Languages({
 	text,
@@ -10,6 +11,11 @@ export default function Languages({
 	languages: string[]
 }) {
 	const frame = useCurrentFrame() - from
+
+	if (frame < 0) 
+		return null
+	
+
 	return {
 		from,
 		background: undefined,
@@ -17,14 +23,23 @@ export default function Languages({
 		content: (
 			<>
 				<h2 className='headline'>{text}</h2>
-				<div className='flex gap-5'>
-					{languages.map((language, i) => (
-						<div
-							style={{opacity: frame > i * 30 ? frame / 30 - i : 0}}
-							className={`text-5xl devicon-${language.toLowerCase()}-plain colored`}
-							key={language}
-						/>
-					))}
+				<div className='inset-shadow flex gap-6 rounded-xl border border-white/20 p-6 text-[#313131]'>
+					{languages.map((language, i) => {
+						return (
+							<div
+								style={{opacity: frame > i * 30 ? frame / 30 - i : 0}}
+								className={`text-6xl devicon-${getLanguage(
+									language
+								)}-plain relative text-white`}
+								key={language}>
+								<div
+									className={`text-6xl devicon-${getLanguage(
+										language
+									)}-plain absolute left-0 top-0 h-full w-full text-white blur-[3px]`}
+								/>
+							</div>
+						)
+					})}
 				</div>
 			</>
 		)
