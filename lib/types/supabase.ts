301c62a226f97dafdecbe86375389fe2db9e2813
Chang Liu <chang@graphite.dev>
2025-01-16 22:51:02 -0500

    fix(types) lint

--- lib/types/supabase.ts
+++ lib/types/supabase.ts
@@ -1,195 +1,195 @@
 export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+	| string
+	| number
+	| boolean
+	| null
+	| {[key: string]: Json | undefined}
+	| Json[]
 
 export interface Database {
-  public: {
-    Tables: {
-      profile: {
-        Row: {
-          avatar_url: string | null
-          company: string | null
-          created_at: string
-          download_size: number | null
-          download_url: string | null
-          email: string | null
-          github_stats: Json | null
-          id: string
-          is_graphite_user: boolean
-          is_public: boolean
-          is_rendered: boolean | null
-          pull_requests_opened: number | null
-          user_name: string
-          video_manifest: Json | null
-        }
-        Insert: {
-          avatar_url?: string | null
-          company?: string | null
-          created_at?: string
-          download_size?: number | null
-          download_url?: string | null
-          email?: string | null
-          github_stats?: Json | null
-          id?: string
-          is_graphite_user?: boolean
-          is_public?: boolean
-          is_rendered?: boolean | null
-          pull_requests_opened?: number | null
-          user_name: string
-          video_manifest?: Json | null
-        }
-        Update: {
-          avatar_url?: string | null
-          company?: string | null
-          created_at?: string
-          download_size?: number | null
-          download_url?: string | null
-          email?: string | null
-          github_stats?: Json | null
-          id?: string
-          is_graphite_user?: boolean
-          is_public?: boolean
-          is_rendered?: boolean | null
-          pull_requests_opened?: number | null
-          user_name?: string
-          video_manifest?: Json | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "profile_id_fkey"
-            columns: ["id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
-    Views: {
-      leaderboard: {
-        Row: {
-          avatar_url: string | null
-          company: string | null
-          is_graphite_user: boolean | null
-          pull_requests_opened: number | null
-          user_name: string | null
-        }
-        Relationships: []
-      }
-    }
-    Functions: {
-      get_profile: {
-        Args: {
-          user_name_input?: string
-          id_input?: string
-        }
-        Returns: {
-          avatar_url: string | null
-          company: string | null
-          created_at: string
-          download_size: number | null
-          download_url: string | null
-          email: string | null
-          github_stats: Json | null
-          id: string
-          is_graphite_user: boolean
-          is_public: boolean
-          is_rendered: boolean | null
-          pull_requests_opened: number | null
-          user_name: string
-          video_manifest: Json | null
-        }[]
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+	public: {
+		Tables: {
+			profile: {
+				Row: {
+					avatar_url: string | null
+					company: string | null
+					created_at: string
+					download_size: number | null
+					download_url: string | null
+					email: string | null
+					github_stats: Json | null
+					id: string
+					is_graphite_user: boolean
+					is_public: boolean
+					is_rendered: boolean | null
+					pull_requests_opened: number | null
+					user_name: string
+					video_manifest: Json | null
+				}
+				Insert: {
+					avatar_url?: string | null
+					company?: string | null
+					created_at?: string
+					download_size?: number | null
+					download_url?: string | null
+					email?: string | null
+					github_stats?: Json | null
+					id?: string
+					is_graphite_user?: boolean
+					is_public?: boolean
+					is_rendered?: boolean | null
+					pull_requests_opened?: number | null
+					user_name: string
+					video_manifest?: Json | null
+				}
+				Update: {
+					avatar_url?: string | null
+					company?: string | null
+					created_at?: string
+					download_size?: number | null
+					download_url?: string | null
+					email?: string | null
+					github_stats?: Json | null
+					id?: string
+					is_graphite_user?: boolean
+					is_public?: boolean
+					is_rendered?: boolean | null
+					pull_requests_opened?: number | null
+					user_name?: string
+					video_manifest?: Json | null
+				}
+				Relationships: [
+					{
+						foreignKeyName: 'profile_id_fkey'
+						columns: ['id']
+						isOneToOne: false
+						referencedRelation: 'users'
+						referencedColumns: ['id']
+					}
+				]
+			}
+		}
+		Views: {
+			leaderboard: {
+				Row: {
+					avatar_url: string | null
+					company: string | null
+					is_graphite_user: boolean | null
+					pull_requests_opened: number | null
+					user_name: string | null
+				}
+				Relationships: []
+			}
+		}
+		Functions: {
+			get_profile: {
+				Args: {
+					user_name_input?: string
+					id_input?: string
+				}
+				Returns: {
+					avatar_url: string | null
+					company: string | null
+					created_at: string
+					download_size: number | null
+					download_url: string | null
+					email: string | null
+					github_stats: Json | null
+					id: string
+					is_graphite_user: boolean
+					is_public: boolean
+					is_rendered: boolean | null
+					pull_requests_opened: number | null
+					user_name: string
+					video_manifest: Json | null
+				}[]
+			}
+		}
+		Enums: {
+			[_ in never]: never
+		}
+		CompositeTypes: {
+			[_ in never]: never
+		}
+	}
 }
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-      Database["public"]["Views"])
-  ? (Database["public"]["Tables"] &
-      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : never
+	PublicTableNameOrOptions extends
+		| keyof (Database['public']['Tables'] & Database['public']['Views'])
+		| {schema: keyof Database},
+	TableName extends PublicTableNameOrOptions extends {schema: keyof Database}
+		? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+				Database[PublicTableNameOrOptions['schema']]['Views'])
+		: never = never
+> = PublicTableNameOrOptions extends {schema: keyof Database}
+	? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+			Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+			Row: infer R
+		}
+		? R
+		: never
+	: PublicTableNameOrOptions extends keyof (Database['public']['Tables'] &
+				Database['public']['Views'])
+		? (Database['public']['Tables'] &
+				Database['public']['Views'])[PublicTableNameOrOptions] extends {
+				Row: infer R
+			}
+			? R
+			: never
+		: never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : never
+	PublicTableNameOrOptions extends
+		| keyof Database['public']['Tables']
+		| {schema: keyof Database},
+	TableName extends PublicTableNameOrOptions extends {schema: keyof Database}
+		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+		: never = never
+> = PublicTableNameOrOptions extends {schema: keyof Database}
+	? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+			Insert: infer I
+		}
+		? I
+		: never
+	: PublicTableNameOrOptions extends keyof Database['public']['Tables']
+		? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+				Insert: infer I
+			}
+			? I
+			: never
+		: never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : never
+	PublicTableNameOrOptions extends
+		| keyof Database['public']['Tables']
+		| {schema: keyof Database},
+	TableName extends PublicTableNameOrOptions extends {schema: keyof Database}
+		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+		: never = never
+> = PublicTableNameOrOptions extends {schema: keyof Database}
+	? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+			Update: infer U
+		}
+		? U
+		: never
+	: PublicTableNameOrOptions extends keyof Database['public']['Tables']
+		? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+				Update: infer U
+			}
+			? U
+			: never
+		: never
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
-    | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never
+	PublicEnumNameOrOptions extends
+		| keyof Database['public']['Enums']
+		| {schema: keyof Database},
+	EnumName extends PublicEnumNameOrOptions extends {schema: keyof Database}
+		? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+		: never = never
+> = PublicEnumNameOrOptions extends {schema: keyof Database}
+	? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+	: PublicEnumNameOrOptions extends keyof Database['public']['Enums']
+		? Database['public']['Enums'][PublicEnumNameOrOptions]
+		: never
