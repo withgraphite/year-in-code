0af40e8c7c62dce17be421337b5963c85ae66565
Chang Liu <chang@graphite.dev>
2025-01-22 14:48:33 -0500

    fix(og images): fix dynamic og route
    (cherry picked from commit ed20d94b4105644346801aaa9620fc46f402de7e)

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,27 @@ export const metadata = {
 }
 
 export default async function Page() {
+<<<<<<< HEAD
 
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+
+=======
+>>>>>>> Stashed changes
+	const supabase = createServerComponentClient<Database>({cookies})
+	const {
+		data: {session}
+	} = await supabase.auth.getSession()
+<<<<<<< Updated upstream
+	if (session) redirect(`/loading`)
+=======
+
+	if (session) redirect(`/loading`)
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> ed20d94 (fix(og images): fix dynamic og route)
 	return (
 		<div className='flex h-screen w-full flex-col items-center justify-center gap-10 p-5'>
 			 <Hero />
