5378ea5317e89cecea3d025dba3032c5a01b340a
Chang Liu <chang@graphite.dev>
2025-01-23 13:43:26 -0500

    feat: page transitions

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -1,14 +1,9 @@
 import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
 import {Metadata} from 'next'
 import {cookies} from 'next/headers'
-import DownloadControls from '~/components/DownloadControls'
-import Player from '~/components/Player'
-import SignInButton from '~/components/SignInButton'
-import Toolbar from '~/components/Toolbar'
+import {ProfilePage} from '~/components/Profile'
 import {META} from '~/constants/metadata'
-import {Stats} from '~/types/github'
 import {Database} from '~/types/supabase'
-import {Manifest} from '~/types/video'
 import getProfile from '~/utils/profile'
 
 export const dynamic = 'force-dynamic'
@@ -54,57 +49,11 @@ export default async function Profile({params}: {params: {username: string}}) {
 	const {data: profile, error} = await getProfile(params.username, session)
 
 	return (
-		<div className='flex h-screen w-full flex-col items-center justify-center gap-5 p-8'>
-			{/* User exists */}
-			{profile && !error && (
-				<div
-					id='videoContainer'
-					className='z-1 relative flex w-full max-w-3xl flex-col justify-center gap-5 text-white sm:items-center lg:mt-10 2xl:mt-0'>
-					<Toolbar
-						username={params.username}
-						profile={profile}
-						session={session}
-					/>
-
-					{profile && (
-						<Player
-							video={profile.video_manifest as Manifest}
-							stats={profile.github_stats as unknown as Stats}
-						/>
-					)}
-					<p className='w-full text-center text-white sm:hidden'>
-						For the complete experience, play on desktop.
-					</p>
-
-					<DownloadControls
-						profile={profile}
-						inputProps={{
-							title: profile.user_name,
-							userId: profile.id,
-							video: profile.video_manifest as Manifest,
-							stats: profile.github_stats as unknown as Stats
-						}}
-					/>
-				</div>
-			)}
-
-			{/* Error handling */}
-			{error && (
-				<div className='z-1 relative flex flex-col items-center justify-center gap-6'>
-					<h3 className='text-pretty text-center text-gray-300'>
-						Oops!{' '}
-						<span className='font-mono font-bold text-gray-200'>
-							@{params.username}
-						</span>{' '}
-						{error === 'Incomplete profile' ? (
-							<span>has an incomplete video. Try again.</span>
-						) : (
-							<span>either does not exist, or has a private video.</span>
-						)}
-					</h3>
-					{!session && <SignInButton />}
-				</div>
-			)}
-		</div>
+		<ProfilePage
+			profile={profile}
+			error={error}
+			username={params.username}
+			session={session}
+		/>
 	)
 }
